52d8235486164ffc9202b735717abcb0c70ca06d
Ellen Arvidsson <ellen.arvidsson@protonmail.com>
2021-04-05 13:11:56 +0200

    stuff

--- src/hs/Main.hs
+++ src/hs/Main.hs
@@ -1,36 +1,61 @@
--- TODO implement IO system
--- TODO set up EPoll
+-- TODO implement socket handling
 
 import Control.Concurrent.MVar
 import Network.Socket
-import EPoll (EPoll, EPollEvt (..), EPollOpt (..), create, register, unregister)
+import Data.ByteString
+import EPoll (EPoll, EPollEvt (..), EPollOpt (..), create, register, unregister, wait)
 
 newtype RdySignal = RdySignal (MVar (Bool, Maybe (MVar ())))
 
 -- Should signal be ready on creation?
 newRdySignal = RdySignal <$> (newMVar (False, Nothing))
 
-data AsyncSocket = AsyncSocket
-    {
-    sock :: Socket,
-    rdyRead :: RdySignal,
-    rdyWrite :: RdySignal
-    }
+data WrappedSocket = AcceptSocket Socket
+                   | AsyncSocket Socket RdySignal RdySignal
 
-wrapSocket :: EPoll AsyncSocket -> Socket -> IO AsyncSocket
+wrapSocket :: EPoll WrappedSocket -> Socket -> IO WrappedSocket
 wrapSocket epoll sock = do
     r <- newRdySignal
     w <- newRdySignal
     let as = AsyncSocket sock r w 
-        sfd = fdSocket sock in do
-        register epoll sfd [EPOLLIN, EPOLLOUT, EPOLLHUP, EPOLLRDHUP, EPOLLERR, EPOLLET, EPOLLPRI] as
-        return as
+        sfd = fdSocket sock in
+        do register epoll sfd [EPOLLIN, EPOLLOUT, EPOLLHUP, EPOLLRDHUP, EPOLLERR, EPOLLET, EPOLLPRI] as
+           return as
 
-killSocket :: EPoll AsyncSocket -> AsyncSocket -> IO ()
-killSocket epoll (AsyncSocket sock rd wr) = do
-    let sfd = fdSocket sock in do
-        unregister epoll sfd
-        close sock
+wrapAccept :: EPoll WrappedSocket -> Socket -> IO WrappedSocket
+wrapAccept epoll sock = do
+    let as = AcceptSocket sock
+        sfd = fdSocket sock in
+        do register epoll sfd [EPOLLIN, EPOLLERR, EPOLLET] as
+           return as
+            
+
+killSocket :: EPoll WrappedSocket -> WrappedSocket -> IO ()
+killSocket epoll wrapped = do
+    let sfd = case wrapped of
+                  AcceptSocket sock    -> fdSocket sock
+                  AsyncSocket sock _ _ -> fdSocket sock in
+        do unregister epoll sfd
+           close sock
+
+asyncRecv :: WrappedSocket -> Int -> IO ByteString
+asyncRecv (AsyncSocket sock rdyRd _) num = do
+    waitRdy rdyRd
+    recv
+
+
+asyncSend :: WrappedSocket -> ByteString -> IO ()
+asyncSend = undefined
+
+handleRequest :: WrappedSocket -> IO ()
+handleRequest sock = do
+    bs <- asyncRecv sock 1024
+    if length bs > 0 then do
+        asyncSend sock bs
+        handleRequest sock
+    else killSocket sock
+    
+
 
 main = do
     putStrLn "Starting server"
@@ -38,5 +63,6 @@ main = do
     asock <- socket AF_INET Stream 0
     bind asock $ SockAddrInet 1234 $ tupleToHostAddress (127, 0, 0, 1)
     listen asock 64
-    -- register to epoll
+    wrapped <- wrapAccept asock
+    
     -- start accepting
